91b575f5754af1080a33e8b18c7611125ea14074
Arun Kumar <arunkumar.aqm@gmail.com>
2020-02-04 00:06:17 +0530

    Add files via upload

---
+++ MullersMethod.py
@@ -0,0 +1,127 @@
+"""
+Implementation of Muller's method
+Approximates the root or at least one of the roots for an equation of the form f(x) = 0
+
+Contributors:
+Archana (most of the code)
+Arun (to a lesser extent)
+"""
+
+from math import *
+
+expr = input("Enter the function: ")
+expr = expr.replace("^", "**") #** is used for pow in Python
+
+#TODO Support for natural mathematic notation
+#TODO Input validation
+
+x0 = float(input("Initial guess 1: "))
+x1 = float(input("Initial guess 2: "))
+x2 = float(input("Initial guess 3: "))
+#TODO If x0 = x1 or x1 = x2, ZeroDivisionError is raised.
+
+func = lambda x: eval(expr) # Assuming "x" is the independent var
+
+while True: #TODO Please limit noof iterations
+    e = func(x0)
+    f = func(x1)
+    g = func(x2)
+
+    h0 = x1-x0
+    h1 = x2-x1
+    delta0 = (f-e) / h0
+    delta1 = (g-f) / h1
+
+    a = (delta1-delta0)/(h1+h0)
+    b = (a*h1)+delta1
+    c = g
+    d = sqrt((b*b)-(4*a*c)) # Square root of discriminant
+    #TODO Support for complex numbers; Keeps throwing ValueError - math domain error
+
+    if b < 0:
+        x3 = x2-((2*c)/(b-d))
+    else:
+        x3 = x2-((2*c)/(b+d))
+
+    rel_percentage_error = abs(((x3-x2)/x3)*100) # Is it really necessary to calculate this when a simple x3 == x2 comparison is all is required? Plus TODO ZeroDivisionError for x3
+
+    if rel_percentage_error == 0:
+        print("One of the roots is: ", x3) #TODO Maybe set precision unless that defeats the point
+        break
+        
+    else:
+        x0, x1, x2 = x1, x2, x3
+
+"""
+TEST CASES
+
+#1
+stdin:
+x^3 - 13*x - 12
+4.5
+5.5
+5.0
+
+stdout:
+4.0
+
+#2
+stdin:
+x^4 - 3*x^2 + x - 10
+2
+2.5
+3
+
+stdout:
+2.1624
+
+We need test cases for non-polynomial functions too.
+"""      
+
+"""
+OLD CODE
+# What's with all the semicolons?
+
+from math import *
+expr = input("Enter the function:");
+x0 = float(input("Intial guess 1:"));
+dict0 = {}
+dict0['x'] = x0;
+x1 = float(input("Intial guess 2:"));
+dict1 = {}
+dict1['x'] = x1;
+x2 = float(input("Intial guess 3:"));
+dict2 = {}
+dict2['x'] = x2;
+while(1):
+    dict0 = {}
+    dict0['x'] = x0;
+    e = eval(expr,dict0);
+    dict1 = {}
+    dict1['x'] = x1;
+    f = eval(expr,dict1);
+    dict2 = {}
+    dict2['x'] = x2;
+    g = eval(expr,dict2);
+    h0 = x1-x0;
+    h1 = x2-x1;
+    delta0 = (f-e) / h0;
+    delta1 = (g-f) / h1;
+    a = (delta1-delta0) / (h1+h0);
+    b = (a*h1)+delta1;
+    c = g;
+    d = sqrt((b*b)-(4*a*c));
+    if b<0:
+        x3 = x2-((2*c) / (b-d));
+    else:
+        x3 = x2-((2*c) / (b+d));
+    error = abs(((x3-x2) / x3)*100);
+    if error =  = 0:
+        print("The root is:",x3)
+        break
+    else:
+        x0 = x1;
+        x1 = x2;
+        x2 = x3;
+
+  """  
